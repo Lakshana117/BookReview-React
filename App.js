ffe21c12ffe1c6348a7ba48c6e8635d3cb8f03b3
Lakshana117 <151196311+Lakshana117@users.noreply.github.com>
2023-12-14 10:02:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Registers from './BookReview/register';
+
+
+function App() {
+  return (
+    <div className="App">
+      < Registers/>
+    
+    </div>
+  );
+}
+
+export default App;
